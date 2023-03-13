bc070eb95ab7bf7451e9ea299e32fcd61871f450
LucasSPeixotoS <lucasxvs5@gmail.com>
2023-03-13 17:20:15 -0300

    Feature: Adicionando Order

---
+++ src/main/java/com/restapi/lp/entities/Order.java
@@ -0,0 +1,69 @@
+package com.restapi.lp.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class Order implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate moment;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User customer;
+
+    public Order() {
+    }
+
+    public Order(Long id, LocalDate moment, User customer) {
+        this.id = id;
+        this.moment = moment;
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getMoment() {
+        return moment;
+    }
+
+    public void setMoment(LocalDate moment) {
+        this.moment = moment;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return id.equals(order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
