78be6ae87afe519454fe0b521ed3eaa010d7bc1d
LucasSPeixotoS <lucasxvs5@gmail.com>
2023-04-08 20:26:51 -0300

    Criando o service de Product

---
+++ src/main/java/com/restapi/lp/services/ProductService.java
@@ -0,0 +1,31 @@
+package com.restapi.lp.services;
+
+import com.restapi.lp.entities.Category;
+import com.restapi.lp.entities.Product;
+import com.restapi.lp.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> category = productRepository.findById(id);
+        if (category.isEmpty()) {
+            return new Product();
+        } else {
+            return category.get();
+        }
+
+    }
+}
